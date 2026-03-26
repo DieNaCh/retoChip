29f25f44a5277a8b45296864115cb9172885a574
Alejandro Guajardo <alexguajardoc0902@gmail.com>
2026-03-26 11:29:26 -0600

    Added interactivity

--- entregable.py
+++ entregable.py
@@ -1,6 +1,9 @@
 import random
 import math
 import csv 
+import time
+import matplotlib
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt  
 
 # Delimitación de valores aleatorios para las variables
@@ -23,9 +26,22 @@ output_file_path = 'values.csv'
 head = ["Velocidad Angular", "Radio de la rueda (m)", "Relacion de Transmision", "RPM"]
 data = [head]
 
-
 # Generar aleatoriamente y almacenar valores de RPM
 rpm_values = []
+iteraciones = []
+
+# Gráfico interactivo
+plt.ion()
+fig, ax = plt.subplots(figsize=(8, 5))
+line, = ax.plot([], [], marker='o', linestyle='-', color='b', label='RPM')
+
+ax.set_xlim(0, num_results)
+ax.set_ylim(0, 30)
+ax.set_title('Valores de RPM Generados Aleatoriamente')
+ax.set_xlabel('Número de Iteración')
+ax.set_ylabel('Revoluciones por Minuto (RPM)')
+ax.grid(True)
+ax.legend()
 
 for i in range(num_results):
     random_radius = random.uniform(min_radius, max_radius)
@@ -39,20 +55,23 @@ for i in range(num_results):
     data.append(data_val)
 
     rpm_values.append(r_RPM)
+    iteraciones.append(i)
+
+    # Generar gráficas a partir de los datos obtenidos
+    line.set_data(iteraciones, rpm_values)
+    
+    # Ajuste automático de los ejes si los valores se salen del rango
+    if r_RPM > ax.get_ylim()[1]:
+        ax.set_ylim(0, r_RPM + 10)
+    
+    plt.draw()
+
+    plt.pause(0.5)
 
 with open(output_file_path, mode = 'w', newline = '') as csv_file:
     # Guardar datos obtenidos en el .csv
     writer = csv.writer(csv_file)
     writer.writerows(data)
 
-    # Generar gráficas a partir de los datos obtenidos
-    plt.figure(figsize=(8, 5))
-    plt.plot(range(1, num_results + 1), rpm_values, marker='o', linestyle='-', color='b', label='RPM')
-
-    plt.title('Valores de RPM Generados Aleatoriamente')
-    plt.xlabel('Numero de Iteración / Prueba')
-    plt.ylabel('Revoluciones Por Minuto (RPM)')
-    plt.grid(True)
-    plt.legend()
-
-    plt.show()
+plt.ioff()
+plt.show()
